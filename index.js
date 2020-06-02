4ef8541c2072ac7f5976af72adb5c85f987b6c3a
Daniel Washburn <5284402+djwashburn@users.noreply.github.com>
2020-06-02 12:34:02 -0400

    draft of code

---
+++ index.js
@@ -0,0 +1,8 @@
+const logvar = (varObject) => {
+  const varName = Object.getOwnPropertyNames(varObject)[0];
+  console.log(varName);
+  console.log(varObject[varName]);
+};
+
+exports.logvar = logvar;
+
